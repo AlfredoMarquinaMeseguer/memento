0d3a0612f79144fffe50de7bf192cda98667b8fc
AlfredoMarquinaMeseguer <alfmarmes@gmail.com>
2023-02-18 20:59:37 +0100

    La gran comentada

--- memento/models/client_info.py
+++ memento/models/client_info.py
@@ -3,27 +3,98 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError
 from odoo.tools.translate import _
 
+""" 
+This file contains the following classes:
+
+:class ClientInfo: Information of the client, specific to the funerary-thanatory
+:class ResPartner: Inheritance from res.partner, representative of the client
+:class Document: Documents need by the client_info
+"""
 
 class ClientInfo(models.Model):
+    """
+    This class contains several attributes related to the processes 
+    the body undergoes during the its time in the funerary-thanatory
+    :ivar _name: Name of the table in the database
+    :vartype _name: str    
+    :ivar _description: Description of the table in the database
+    :vartype _description: str
+    :ivar name: Name of the client
+    :vartype name: odoo.fields.Char    
+    :ivar thantopraxy_notes: Specifications for the thanatopraxor
+    :vartype name: odoo.fields.Char    
+    :ivar category: Plan which the client has hired
+    :vartype category: odoo.fields.Selection    
+    :ivar state: Stage of the funerary cicle
+    :vartype state: odoo.fields.Selection    
+    :ivar hired_services: Services hired by the client
+    :vartype hired_services: odoo.fields.Many2many    
+    :ivar representative_id: Living representative(s) of the client
+    :vartype representative_id: odoo.fields.Many2many        
+    :ivar required_documents: Documents required by the client in order 
+                              to be buried and others
+    :vartype required_documents: odoo.fields.One2Many    
+    :ivar room_id: Room where the velatory will be held
+    :vartype room_id: odoo.fields.Many2one    
+    :ivar start_booking: Moment when the client will enter the velatory 
+                         room
+    :vartype start_booking: odoo.fields.DateTime    
+    :ivar end_booking: Moment when the client will leave the velatory 
+                       roomName of the client
+    :vartype end_booking: odoo.fields.DateTime     
     
-    """Client Info
-
     :raises UserError: when trying an illegal transition of state 
-    """    
+    """
+
     _name = 'memento.client.info'
     _description = 'Client Info'
 
     COLLECT = 'to_collect'
+    """
+    State value for when the client hasn't been retive from the morgue yet.
+    
+    :type: str 
+    """
     PRE_THANATOS = 'pre_thanatos'
+    """
+    State value for when the client hasn't enter thanatropraxy yet.
+    
+    :type: str 
+    """
     IN_THANATOS = 'in_thanatos'
+    """
+    State value for when the client is in thanatopraxy at the moment.
+    
+    :type: str 
+    """
     NO_ROOM = 'no_room'
+    """
+    State value for when the client has finnished thantopraxy but hasn't been
+    assigned a room yet.
+    
+    :type: str 
+    """
     IN_ROOM = 'in_room'
+    """
+    State value for when the client is in a velatory room at the moment.
+    
+    :type: str 
+    """
     TO_SHIP = 'to_ship'
+    """
+    State value for when the client has finnished the velatory but hasn't 
+    leave the funerary-thanatory yet.
+    
+    :type: str 
+    """
     SHIPPED = 'shipped'
+    """
+    State value for when the client has left the funerary-thanatory yet.
     
-    
-    name = fields.Char(required=True, string="Name")
+    :type: str 
+    """
 
+    name = fields.Char(required=True, string="Name")
     thantopraxy_notes = fields.Text('Information for the thanatopraxor')
     category = fields.Selection(
         [('gold', 'Gold Plan'),
@@ -80,7 +151,8 @@ class ClientInfo(models.Model):
         :type new_state: str
 
         :returns: whether the transition is allowed or not
-        :rtype: bool"""
+        :rtype: bool
+        """
         # List of allowed transitions
         allowed = [(ClientInfo.COLLECT, ClientInfo.PRE_THANATOS),
                    (ClientInfo.COLLECT, ClientInfo.IN_THANATOS),
@@ -94,9 +166,10 @@ class ClientInfo(models.Model):
         return (current_state, new_state) in allowed
 
     def change_state(self, new_state):
-        """Changes the current state of self to a new_state if allowed
+        """Changes the current state of self to a new_state
 
-        :raises UserError: if transition illegal
+        If allowed reassings the state of the client to the new state. If not allowed raises an UserError
+        :raises UserError: illegal transition 
         """
         for body in self:
             if body.is_allowed_transition(body.state, new_state):
@@ -106,7 +179,7 @@ class ClientInfo(models.Model):
                     body.state, new_state)
                 raise UserError(message)
 
-    def collect(self):        
+    def collect(self):
         """Changes the state of the client to PRE_THANATOS
 
         Used when the body has already arrived to the thanatory-funerary.
@@ -150,17 +223,18 @@ class ClientInfo(models.Model):
 
 
 class ResPartner(models.Model):
-    """_summary_
+    """Client Representative
 
-    :param models: _description_
-    :type models: _type_
+    The client representative is the living person the funerary-thanatory can contact to arragements notify of the state of the client.
+    The client representative is technically the real client.
+    :ivar _inherit: Name of the table it inherits from
+    :vartype _inherit: str
+    :ivar represented_client: Clientes representados
+    :vartype represented_client: odoo.fields.Many2many
+    :param models: class that allows this class to become a table in the postgress database
+    :type models: object
     """
-      
-    """_summary_
-    """      
     _inherit = 'res.partner'
-    """_summary_
-    """
     represented_client = fields.Many2many(
         'memento.client.info',
         string='Represented Client'
@@ -168,7 +242,25 @@ class ResPartner(models.Model):
 
 
 class Document(models.Model):
-
+    """Document
+    
+    Documents needed by the client needs in order to: be able to be buried, allow the heirs to have the inheritance,
+    transport the body between states, provinces or even countries, etc.
+    :ivar _name: Name of the table in the database
+    :vartype _name: str    
+    :ivar _description: Description of the table in the database
+    :vartype _description: str
+    :param models: class that allows this class to become a table in the postgress database
+    :type models: object
+    :ivar name: Name of the document being presented
+    :vartype name: odoo.fields.Char
+    :ivar client: Client which the document belogns to
+    :vartype client: odoo.fields.Many2one
+    :ivar description: Description of the content of the document
+    :vartype description: odoo.fields.Text
+    :ivar route: /path/to/document
+    :vartype route: odoo.fields.Text
+    """
     _name = 'memento.document'
     _description = 'Documents related'
 
--- memento/models/room.py
+++ memento/models/room.py
@@ -5,17 +5,55 @@ from odoo.tools.translate import _
 
 
 class Room(models.Model):
+    """Velatory room
+
+    :ivar _name: Name of the table in the database
+    :vartype _name: str    
+    :ivar _description: Description of the table in the database
+    :vartype _description: str
+    :ivar name: Name of the room
+    :vartype name: odoo.fields.Char
+    :ivar room_number: Number of the room
+    :vartype room_number: odoo.fields.Integer
+    :ivar state: State of the room
+    :vartype state: odoo.fields.Selection
+    :ivar booking: List of clients who have booked the room
+    :vartype booking: odoo.fields.One2many
+    """
     _name = 'memento.room'
     _description = 'Room Booking'
 
-    # _parent_store = True
-    # _parent_name = "parent_id"  # optional if field is 'parent_id'
-
+    # A la próxima importo enum
     FREE = 'free'
+    """
+    State value for when the room is free
+    
+    :type: str 
+    """
     BOOKED = 'booked'
+    """
+    State value for when the room is booked
+    
+    :type: str 
+    """
     OCCUPIED = 'occupied'
+    """
+    State value for when the room is occupied
+    
+    :type: str 
+    """
     DIRTY = 'dirty'
+    """
+    State value for when the room is dirty
+    
+    :type: str 
+    """
     MAINTENANCE = 'maintenance'
+    """
+    State value for when the room is under maintenance
+    
+    :type: str 
+    """
 
     name = fields.Char(required=True, string="Name")
     room_number = fields.Integer('Room number')
@@ -35,7 +73,12 @@ class Room(models.Model):
     )
 
     @api.model
-    def is_allowed_transition(self, old_state, new_state):
+    def is_allowed_transition(self, old_state: str, new_state: str) -> bool:
+        """Changes the current state of self to a new_state
+
+        If allowed reassings the state of the room to the new state. If not allowed raises an UserError
+        :raises UserError: illegal transition 
+        """
         allowed = [(Room.FREE, Room.BOOKED),
                    (Room.FREE, Room.OCCUPIED),
                    (Room.FREE, Room.MAINTENANCE),
@@ -50,7 +93,15 @@ class Room(models.Model):
                    (Room.MAINTENANCE, Room.FREE)]
         return (old_state, new_state) in allowed
 
-    def change_state(self, new_state):
+    def change_state(self, new_state: str):        
+        """Changes the current state of the room to new_state
+
+        If allowed reassings the state of the client to the new state. 
+        If not allowed raises an UserError
+        :param new_state: State that wants to be transitioned to
+        :type new_state: str
+        :raises UserError: illegal transition 
+        """
         for room in self:
             if room.is_allowed_transition(room.state, new_state):
                 room.state = new_state
@@ -60,16 +111,26 @@ class Room(models.Model):
                 raise UserError(message)
 
     def make_free(self):
+        """Changes the state of the room to FREE"""
+        
         self.change_state(Room.FREE)
 
     def make_booked(self):
+        """Changes the state of the room to BOOKED"""
+        
         self.change_state(Room.BOOKED)
 
     def make_occupied(self):
+        """Changes the state of the room to OCCUPIED"""
+        
         self.change_state(Room.OCCUPIED)
 
     def make_dirty(self):
+        """Changes the state of the room to DIRTY"""
+        
         self.change_state(Room.DIRTY)
 
     def make_maintenance(self):
+        """Changes the state of the room to MAINTENANCE"""
+        
         self.change_state(Room.MAINTENANCE)
